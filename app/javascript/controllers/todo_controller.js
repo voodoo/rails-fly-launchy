cc81bcc2cff0625f39ad1478fd65a56c51686a34
Paul Vudmaska <paul@vudmaska.com>
2024-10-14 10:13:07 -0500

    fixed

---
+++ app/javascript/controllers/todo_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["title", "input", "submit", "form", "checkbox"]
+
+  connect() {
+    console.log("TodoController connected");
+  }
+
+  edit(event) {
+    console.log("edit");
+    event.preventDefault();
+    this.titleTarget.classList.add("hidden")
+    this.inputTarget.classList.remove("hidden")
+    this.submitTarget.classList.remove("hidden")
+    this.inputTarget.focus()
+    this.inputTarget.select()
+    this.inputTarget.addEventListener("blur", this.update.bind(this))
+  }
+
+  update(event) {
+    event.preventDefault()
+    this.formTarget.submit()
+  }
+
+  toggleComplete(event) {
+   // this.checkboxTarget.disabled = true
+    this.formTarget.submit()
+  }
+}
