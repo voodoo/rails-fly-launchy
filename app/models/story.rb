66e2f1159d89a7224977b864feb063a59203ce6c
Paul Vudmaska <paul@vudmaska.com>
2024-10-15 16:01:34 -0500

    story fix protocol

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,7 @@ class Story < ApplicationRecord
   belongs_to :user
   has_many :votes, dependent: :destroy
   validates :title, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, uniqueness: true
   validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
   after_commit :vote_for_new_story, on: :create   
 
